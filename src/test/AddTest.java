72e6866813186d4c467d25084d56c784ce536022
ravitejamadala5 <ravitejamadala5@gmail.com>
2021-10-15 20:11:51 +0530

    Empty string handled

--- src/test/AddTest.java
+++ src/test/AddTest.java
@@ -2,7 +2,6 @@ package test;
 import static main.Add.*;
 import static org.junit.Assert.assertEquals;
 
-import main.Add;
 import org.junit.Test;
 
 import org.junit.jupiter.api.Assertions;
@@ -10,7 +9,7 @@ import org.junit.jupiter.api.Assertions;
 
 public class AddTest {
    @Test
-    void return_zero_for_empty_input(){
+   public void return_zero_for_empty_input(){
        Assertions.assertEquals(add(""), 0);
    }
 
